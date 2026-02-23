f55b1e04babc1486632ebf6c3272e1627b574083
pa558 <badepayal25@gmail.com>
2026-02-23 21:56:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function setEnglish() {
+    document.getElementById("langText").innerText =
+        "All sports accessories available here";
+}
+
+function setMarathi() {
+    document.getElementById("langText").innerText =
+        "सर्व प्रकारचे क्रीडा साहित्य येथे उपलब्ध आहे";
+}
